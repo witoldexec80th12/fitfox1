92e2981295e2fb843a0df3f9a02c37a066b6ed0e
rapidbenjamin <rapidbenjamin@gmail.com>
2024-09-16 22:41:52 +1000

    Add 2 pages simple

---
+++ client/src/pages/FriendPage/FriendPage.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const FriendPage:FC = () => {
+
+    return (
+        <div>
+            Welcome Friend Page!
+            This page is on development.
+            It coming soon.
+        </div>
+    )
+}
+
+export default FriendPage;
---
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const ProfilePage:FC = () => {
+
+    return (
+        <div>
+            Welcome Profile Page!
+            This page is on development.
+            It coming soon.
+        </div>
+    )
+}
+
+export default ProfilePage;
