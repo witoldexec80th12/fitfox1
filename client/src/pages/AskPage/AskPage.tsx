8e111afde3189857559dd1f45ef5ad8284a11de0
crypto523 <jacksoncurry46@gmail.com>
2024-09-29 19:00:58 +1000

    Add Twise iframe & fix svg issues

--- client/src/pages/AskPage/AskPage.tsx
+++ client/src/pages/AskPage/AskPage.tsx
@@ -1,69 +1,17 @@
-import React, { useState } from "react";
-
-import ChatMessage from "../../components/ChatMessage/ChatMessage";
-import InputBox from "../../components/InputBox/InputBox";
-import Notification from "../../components/Notification/Notification";
+import React from "react";
 import "./askPage.scss";
-import RecordModal from "../../components/Modal/RecordModal";
 
 const AskPage: React.FC = () => {
-  const [messages, setMessages] = useState<
-    { content: string; isQuestion: boolean; time: string }[]
-  >([]);
-  const [notification, setNotification] = useState(
-    "Ask us any question, as simple or as complex, and our team of expert nutritionists, and our proprietary technology will make sure you get an answer. The more questions you ask, the more points you will get."
-  );
-  const [showRecordModal, setShowRecordModal] = useState<boolean>(false);
-
-  const handleSendMessage = (message: string) => {
-    const time = new Date().toLocaleTimeString();
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { content: message, isQuestion: true, time },
-    ]);
-    setTimeout(() => {
-      fitFoxAnswer();
-    }, 2000);
-  };
-
-  const fitFoxAnswer = () => {
-    const time = new Date().toLocaleTimeString();
-    const message = "This is the answer!";
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { content: message, isQuestion: false, time },
-    ]);
-  };
-
-  const closeNotification = () => setNotification("");
-
   return (
     <div className="chat-page">
-      {/* Header */}
-      <div className="header">
-        <h1>Ask</h1>
-      </div>
-
-      {notification && (
-        <Notification message={notification} onClose={closeNotification} />
-      )}
-
-      {/* Chat Content */}
-      <div className="chat-container">
-        {messages.map((msg, index) => (
-          <ChatMessage
-            key={index}
-            message={msg.content}
-            isQuestion={msg.isQuestion}
-            time={msg.time}
-          />
-        ))}
-      </div>
-
-      {/* Input Box */}
-      <InputBox onSendMessage={handleSendMessage} onClickMic={() => setShowRecordModal(true)} />
-
-      {showRecordModal && <RecordModal onClose={() => setShowRecordModal(false)}/>}
+      <iframe
+        src="https://app.twise.ai/cgm-try-1-w5kppv"
+        width="100%"
+        height="100%"
+        style={{ border: "0" }}
+        allow="microphone"
+        title="Twise Widget"
+      ></iframe>
     </div>
   );
 };
